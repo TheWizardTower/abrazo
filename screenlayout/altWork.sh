1f7900b4b9aff7cec3082584fa5c8ff8b8f38d21
Adam McCullough <merlinfmct87@gmail.com>
2017-05-18 19:04:02 -0700

    New layout, woot.

--- screenlayout/altWork.sh
+++ screenlayout/altWork.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VGA-0 --primary --mode 1920x1200 --pos 0x0 --rotate left --output DP-6 --mode 2560x1440 --pos 1200x0 --rotate left --output DP-5 --mode 3840x2160 --pos 2640x0 --rotate left --output DP-4 --mode 3840x2160 --pos 4800x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
+xrandr --output VGA-0 --off --output DP-6 --primary --mode 2560x1440 --pos 2160x1280 --rotate left --output DP-5 --mode 3840x2160 --pos 0x0 --rotate left --output DP-4 --mode 3840x2160 --pos 3600x1680 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
