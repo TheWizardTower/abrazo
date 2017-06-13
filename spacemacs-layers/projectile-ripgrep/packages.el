5457ef9c95c3252bbf672653ebe777212956a19a
Adam McCullough <merlinfmct87@gmail.com>
2017-06-13 16:28:23 -0700

    Hey guess what I figured out an emacs thing.

--- spacemacs-layers/projectile-ripgrep/packages.el
+++ spacemacs-layers/projectile-ripgrep/packages.el
@@ -4,6 +4,11 @@
 
 (defun projectile-ripgrep/init-projectile-ripgrep ()
   (use-package projectile-ripgrep)
+  (with-eval-after-load 'helm
+    (spacemacs/declare-prefix "sr" "projectile-ripgrep")
+    (spacemacs/set-leader-keys "srp" 'projectile-ripgrep)
+    (define-key projectile-command-map (kbd "s r") 'projectile-ripgrep)
+    )
   )
 
 
