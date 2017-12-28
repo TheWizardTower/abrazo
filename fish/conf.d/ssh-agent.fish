502b5b2f5ee66135a959094f6798cfb66868755d
Adam McCullough <merlinfmct87@gmail.com>
2017-12-27 20:49:58 -0800

    Adding bitbucket key to ssh-agent login prompt.

--- fish/conf.d/ssh-agent.fish
+++ fish/conf.d/ssh-agent.fish
@@ -9,14 +9,14 @@ function start_agent
     echo "succeeded"
     chmod 600 $SSH_ENV
     source $SSH_ENV
-    ssh-add ~/.ssh/ ~/.ssh/github
+    ssh-add ~/.ssh/ ~/.ssh/github ~/.ssh/bitbucket
     ln -sf $SSH_AUTH_SOCK $SSH_SOCK_LINK
 end
 
 function test_identities
     ssh-add -l | grep -e "The agent has no identities" >/dev/null
     if test $status -eq 0
-        ssh-add ~/.ssh/github
+        ssh-add ~/.ssh/github ~/.ssh/bitbucket
         if [ $status -eq 2 ]
             start_agent
         end
