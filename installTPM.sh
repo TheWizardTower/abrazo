700828be1365b37a611d5e792eb2d21f1c2f51ae
Adam McCullough <merlinfmct87@gmail.com>
2017-12-29 00:45:42 -0800

    This is done by Ansible now, so the scripts are just clutter.

--- installTPM.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -d "$HOME/.tmux/plugins/tpm" ]; then
-    cd "$HOME/.tmux/plugins/tpm" || exit
-    git pull
-else
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
