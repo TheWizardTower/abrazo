9de280f0e0ee0aed1252abb22f65993c72ba27d5
Adam McCullough <merlinfmct87@gmail.com>
2018-01-05 16:04:48 -0800

    Adding the setup script I got from the xmonad blog:
    http://brianbuccola.com/how-to-install-xmonad-and-xmobar-via-stack/

---
+++ xmonad/setup.sh
@@ -0,0 +1,3 @@
+git clone "https://github.com/xmonad/xmonad" xmonad-git
+git clone "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
+git clone "https://github.com/jaor/xmobar" xmobar-git
