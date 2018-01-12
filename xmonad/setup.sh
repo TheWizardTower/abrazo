566c38457e90a057de90c8145d1816c11ba2a5cd
Adam McCullough <merlinfmct87@gmail.com>
2018-01-11 22:01:59 -0800

    Linux complained that this wasn't there. :)

--- xmonad/setup.sh
+++ xmonad/setup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 git clone "https://github.com/xmonad/xmonad" xmonad-git
 git clone "https://github.com/xmonad/xmonad-contrib" xmonad-contrib-git
 git clone "https://github.com/jaor/xmobar" xmobar-git
