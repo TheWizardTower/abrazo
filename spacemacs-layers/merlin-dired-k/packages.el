d79c003f5073ea4e3570ca31305f35442f1be917
Adam McCullough <merlinfmct87@gmail.com>
2018-01-23 23:49:13 -0800

    Add some commented code for convenience later.

--- spacemacs-layers/merlin-dired-k/packages.el
+++ spacemacs-layers/merlin-dired-k/packages.el
@@ -4,6 +4,10 @@
 (defun merlin-dired-k/init-dired-k ()
   (use-package dired-k
     :config
+    ;; (define-key dired-mode-map (kbd "K") 'dired-k)
+
+    ;; You can use dired-k alternative to revert-buffer
+    ;; (define-key dired-mode-map (kbd "g") 'dired-k)
     ;; always execute dired-k when dired buffer is opened
     (add-hook 'dired-mode-hook 'dired-k)
     (add-hook 'dired-initial-position-hook 'dired-k)
