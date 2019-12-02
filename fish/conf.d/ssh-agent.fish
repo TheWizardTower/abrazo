55c23818c2066d25904a670bee620e816cdfb4da
Adam McCullough <merlinfmct87@gmail.com>
2019-12-01 21:59:38 -0800

    As-is, bash and fish were fighting over who had ssh auth. Fix!
    The real issue was that bash and fish use different syntax styles (bourne or
    csh), so when you would eval the env var, one or the other would fail, start a
    new one, and clobber the environment variable. No good.

--- fish/conf.d/ssh-agent.fish
+++ fish/conf.d/ssh-agent.fish
@@ -1,6 +1,6 @@
 # These are set here, rather than in paths.d/ because they weren't reliably getting created before this was run.
-set -gx SSH_ENV $HOME/.ssh/environment
-set -gx SSH_SOCK_LINK $HOME/.ssh/auth_sock_symlink
+set -gx SSH_ENV $HOME/.ssh/environment_fish
+set -gx SSH_SOCK_LINK $HOME/.ssh/auth_sock_symlink_fish
 
 function start_agent
     fish echo "Initializing new SSH agent ..."
@@ -14,7 +14,7 @@ function start_agent
 end
 
 function test_identities
-    ssh-add -l | grep -e "The agent has no identities" >/dev/null
+    ssh-add -l | grep -qe "The agent has no identities" >/dev/null
     if test $status -eq 0
         ssh-add ~/.ssh/github ~/.ssh/bitbucket
         if [ $status -eq 2 ]
@@ -32,7 +32,7 @@ set -l output
 if test -z (hostname | grep lin[[:alpha:]]-sandbox)
     if test -n "$SSH_AGENT_PID"
         echo "Passed ssh-agent-pid guard."
-        ps -ef | grep "$SSH_AGENT_PID" | grep ssh-agent >/dev/null
+        ps -ef | grep -q "$SSH_AGENT_PID" | grep -q ssh-agent
         if [ $status -eq 0 ]
             test_identities
         end
@@ -40,7 +40,7 @@ if test -z (hostname | grep lin[[:alpha:]]-sandbox)
         if test -f $SSH_ENV
             source $SSH_ENV >/dev/null
         end
-        ps -ef | grep $SSH_AGENT_PID | grep [s]sh-agent >/dev/null
+        ps -ef | grep -q $SSH_AGENT_PID | grep -q [s]sh-agent
         if test $status -eq 0
             test_identities
         else
