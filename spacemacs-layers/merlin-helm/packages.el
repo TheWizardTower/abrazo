74846749a72bdfe7e5efd0a399662e8844241b53
Adam McCullough <merlinfmct87@gmail.com>
2020-04-13 13:21:31 -0700

    helm goodness!

--- spacemacs-layers/merlin-helm/packages.el
+++ spacemacs-layers/merlin-helm/packages.el
@@ -1,4 +1,28 @@
-(defconst merlin-helm-packages '(helm-file-preview helm-flycheck helm-flyspell helm-git-files helm-git-grep helm-helm-commands helm-ispell helm-lastpass helm-ls-git helm-proc))
+(defconst merlin-helm-packages '(helm-cmd-t helm-describe-modes helm-file-preview helm-flycheck helm-flyspell helm-git-files helm-git-grep helm-helm-commands helm-ispell helm-lastpass helm-ls-git helm-proc))
+
+(defun merlin-helm/init-helm-ag-r ()
+  (use-package 'helm-ag-r
+    :config
+    ;; Specify your favorite ag's configuration
+    ;; You can change below option by pushing C-o on helm-ag-r's minibuffer.
+    (setq helm-ag-r-option-list '("-S -U --hidden" "-S -U -g")
+          ;; start searching from 3 character
+          helm-ag-r-requires-pattern 3
+          ;; delay searching from input
+          helm-ag-r-input-idle-delay 0.5)
+    )
+  )
+
+(defun merlin-helm/init-helm-cmd-t ()
+  (use-package helm-cmd-t)
+  )
+
+(defun merlin-helm/init-helm-describe-modes ()
+  (use-package helm-describe-modes
+    :config
+    (global-set-key (kbd "<f1> m") #'helm-describe-modes)
+    )
+  )
 
 (defun merlin-helm/init-helm-file-preview ()
   (use-package helm-file-preview
