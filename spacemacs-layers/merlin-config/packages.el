430c51ba0bc23e28fa3b313f38a9c11e7a1ea386
Adam McCullough <merlinfmct87@gmail.com>
2020-05-03 02:27:46 -0700

    Add this package.

--- spacemacs-layers/merlin-config/packages.el
+++ spacemacs-layers/merlin-config/packages.el
@@ -1,9 +1,16 @@
-(defconst merlin-config-packages '(fortune-cookie))
+(defconst merlin-config-packages '(fortune-cookie modern-fringes))
 
 (defun merlin-config/init-fortune-cookie ()
   (use-package fortune-cookie
     :config
     (setq fortune-cookie-cowsay-args  "-f tux -s")
     (fortune-cookie-mode)
+    )
+  )
+
+(defun merlin-config/init-modern-fringes ()
+  (use-package modern-fringes
+    :config
+    (modern-fringes-invert-arrows)
     )
   )
