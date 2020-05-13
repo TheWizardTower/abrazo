ce34ee22d842273fff067365ff4d5cc8441d6857
Adam McCullough <merlinfmct87@gmail.com>
2020-05-13 03:51:27 -0700

    bash-completion goodness.

--- spacemacs-layers/merlin-shell/packages.el
+++ spacemacs-layers/merlin-shell/packages.el
@@ -1,4 +1,11 @@
-(defconst merlin-shell-packages '(shfmt))
+(defconst merlin-shell-packages '(bash-completion shfmt))
+
+(defun merlin-shell/init-bash-completion ()
+  (use-package bash-completion
+    :config
+    (bash-completion-setup)
+    )
+  )
 
 (defun merlin-shell/init-shfmt ()
   (use-package shfmt)
