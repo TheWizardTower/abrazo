65c84b4da9863ea175b01ffb48dac4a7a14e3c20
Adam McCullough <merlinfmct87@gmail.com>
2020-09-15 18:51:55 -0700

    Remove eshell-did-you-mean

--- spacemacs-layers/merlin-eshell/packages.el
+++ spacemacs-layers/merlin-eshell/packages.el
@@ -1,11 +1,6 @@
 (defconst merlin-eshell-packages
-  '(eshell-did-you-mean
-    eshell-fringe-status
+  '(eshell-fringe-status
     eshell-git-prompt))
-
-(defun merlin-eshell/init-eshell-did-you-mean ()
-  (use-package eshell-did-you-mean)
-  (eshell-did-you-mean-setup))
 
 (defun merlin-eshell/init-eshell-fringe-status ()
   (use-package eshell-fringe-status)
