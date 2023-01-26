8d3e734b27af2f4c563ff7d634bdcb6133fd54b1
Adam McCullough <merlinfmct87@gmail.com>
2023-01-26 15:28:36 -0800

    Add config files, and get fish hybrid vi/emacs mode set up.

---
+++ fish/conf.d/budspencer.fish
@@ -0,0 +1,22 @@
+function fish_hybrid_key_bindings --description \
+"Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings fish_hybrid_key_bindings
+
+bind '#' __budspencer_toggle_symbols
+bind -M visual '#' __budspencer_toggle_symbols
+bind ' ' __budspencer_toggle_pwd
+bind -M visual ' ' __budspencer_toggle_pwd
+bind L __budspencer_cd_next
+bind H __budspencer_cd_prev
+bind m mark
+bind M unmark
+bind . __budspencer_edit_commandline
+bind -M insert \r __budspencer_preexec
+bind \r __budspencer_preexec
+
+
