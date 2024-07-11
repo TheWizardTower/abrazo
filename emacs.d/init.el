68b9026d1ac335ee9ae6bfb87193d95b5721d66a
Adam McCullough <merlinfmct87@gmail.com>
2024-07-10 18:57:18 -0700

    Auto-formatting goodness for emacs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -232,6 +232,13 @@ If the new path's directories does not exist, create them."
   (setq fortune-cookie-cowsay-args  "-f tux -s")
   (fortune-cookie-mode))
 
+;; auto-format different source code files extremely intelligently
+;; https://github.com/radian-software/apheleia
+(use-package apheleia
+  :ensure t
+  :config
+  (apheleia-global-mode +1))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;;   Built-in customization framework
@@ -253,4 +260,19 @@ If the new path's directories does not exist, create them."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(evil-ex-substitute-matches ((t (:inherit diff-removed :foreground unspecified :background unspecified))))
+ '(evil-ex-substitute-replacement ((t (:inherit diff-added :foreground unspecified :background unspecified))))
+ '(evil-traces-change ((t (:inherit diff-removed))))
+ '(evil-traces-copy-preview ((t (:inherit diff-added))))
+ '(evil-traces-copy-range ((t (:inherit diff-changed))))
+ '(evil-traces-delete ((t (:inherit diff-removed))))
+ '(evil-traces-global-match ((t (:inherit diff-added))))
+ '(evil-traces-global-range ((t (:inherit diff-changed))))
+ '(evil-traces-join-indicator ((t (:inherit diff-added))) t)
+ '(evil-traces-join-range ((t (:inherit diff-changed))))
+ '(evil-traces-move-preview ((t (:inherit diff-added))))
+ '(evil-traces-move-range ((t (:inherit diff-removed))))
+ '(evil-traces-normal ((t (:inherit diff-changed))))
+ '(evil-traces-shell-command ((t (:inherit diff-changed))))
+ '(evil-traces-substitute-range ((t (:inherit diff-changed))))
+ '(evil-traces-yank ((t (:inherit diff-changed)))))
