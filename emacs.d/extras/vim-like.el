5ac1235801db89379888e39b312034a9c8a07200
Adam McCullough <merlinfmct87@gmail.com>
2024-10-19 16:39:24 -0700

    Formatting + crash fixes

--- emacs.d/extras/vim-like.el
+++ emacs.d/extras/vim-like.el
@@ -33,20 +33,21 @@
   ;; Configuring initial major mode for some modes
   (evil-set-initial-state 'vterm-mode 'emacs))
 
-(use-package evil-args)
+(use-package evil-args
+  :config
 
-;; bind evil-args text objects
-(define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
-(define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
+  ;; bind evil-args text objects
+  ;;(define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
+  ;;(define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
 
-;; bind evil-forward/backward-args
-(define-key evil-normal-state-map "L" 'evil-forward-arg)
-(define-key evil-normal-state-map "H" 'evil-backward-arg)
-(define-key evil-motion-state-map "L" 'evil-forward-arg)
-(define-key evil-motion-state-map "H" 'evil-backward-arg)
+  ;; bind evil-forward/backward-args
+  (define-key evil-normal-state-map "L" 'evil-forward-arg)
+  (define-key evil-normal-state-map "H" 'evil-backward-arg)
+  (define-key evil-motion-state-map "L" 'evil-forward-arg)
+  (define-key evil-motion-state-map "H" 'evil-backward-arg)
 
-;; bind evil-jump-out-args
-(define-key evil-normal-state-map "K" 'evil-jump-out-args)
+  ;; bind evil-jump-out-args
+  (define-key evil-normal-state-map "K" 'evil-jump-out-args))
 
 ;;; evil-escape configuration
 
