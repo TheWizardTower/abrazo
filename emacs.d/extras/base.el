6e6f92c57069b670743f6f6bb24db67d32d4a912
Adam James McCullough <merlinfmct87@gmail.com>
2025-04-07 17:35:11 -0700

    Strip out stray whitespace from my files, please

--- emacs.d/extras/base.el
+++ emacs.d/extras/base.el
@@ -240,3 +240,26 @@
   :config
   (telephone-line-mode 1)
   )
+
+(use-package stripspace
+  :ensure t
+  ;; enable for prog-mode-hook, text-mode-hook, conf-mode-hook
+  :hook ((prog-mode . stripspace-local-mode)
+	 (text-mode . stripspace-local-mode)
+	(conf-mode . stripspace-local-mode))
+
+  :custom
+  ;; the `stripspaceonly-if-initially-clean' option:
+  ;; - nil to always delete trailing whitespace
+  ;; - Non-nil to only delete whitespace when the buffer is clean initially.
+  ;; (The initial cleanliness check is performed when `stripspace-local-mode'
+  ;; is enabled)
+  (stripspace-only-if-initially-clean nil)
+
+  ;; Enabling `stripspace-restore-column' preserves the cursor's column position
+  ;; even after stripping spaces. This is useful in scenarios where you add
+  ;; extra spaces and save the file. Although the spaces are removed in the
+  ;; saved file, the cursor remains in the same position, ensuring a consistent
+  ;; editing experience without affecting cursor position.
+  (stripspace-restore-column t)
+  )
