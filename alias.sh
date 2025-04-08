1aea1b5b0c659699042de3b351039ca569b75c98
Adam James McCullough <merlinfmct87@gmail.com>
2025-04-07 18:08:43 -0700

    Alias vim to the flatpak build, conditionally

--- alias.sh
+++ alias.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 alias proot='while (! test -d ./.git) do cd ../; done'
+
+if which flatpak && ! which nvim; then
+	alias nvim="flatpak run io.neovim.nvim"
+fi
+
