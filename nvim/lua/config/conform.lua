2e851c7e1ea566f223a2aff59f0cd87717fd4a11
Adam James McCullough <merlinfmct87@gmail.com>
2025-08-12 10:32:30 -0700

    Add auto-formatting and telescope.

---
+++ nvim/lua/config/conform.lua
@@ -0,0 +1,19 @@
+require("conform").setup({
+    formatters_by_ft = {
+        lua = { "stylua" },
+        -- Conform will run multiple formatters sequentially
+        python = { "isort", "black" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        rust = { "rustfmt", lsp_format = "fallback" },
+        -- Conform will run the first available formatter
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        terraform = { "terraform fmt", stop_after_first = true },
+        bash = { "shfmt", "beautysh", stop_after_first = true },
+        json = { "jq", stop_after_first = true },
+    },
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+    },
+})
