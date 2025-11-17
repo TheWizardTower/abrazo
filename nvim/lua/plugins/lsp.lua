03d7729ad10f138a693d581d45470871a4ce2b22
Adam James McCullough <merlinfmct87@gmail.com>
2025-11-16 21:15:37 -0800

    Remove duplicate config entry.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -44,14 +44,6 @@ return {
         end
     },
     {
-        'junnplus/lsp-setup.nvim',
-        dependencies = {
-            'neovim/nvim-lspconfig',
-            'williamboman/mason.nvim',           -- optional
-            'williamboman/mason-lspconfig.nvim', -- optional
-        },
-    },
-    {
         "williamboman/mason.nvim"
     },
     { "neoclide/coc.nvim" },
