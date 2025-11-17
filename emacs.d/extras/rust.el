4011dbe7d203d90cdd5ecf7540ac242a217b057d
Adam James McCullough <merlinfmct87@gmail.com>
2025-11-17 08:14:34 -0800

    Fix a typo.

--- emacs.d/extras/rust.el
+++ emacs.d/extras/rust.el
@@ -9,7 +9,7 @@
 	      ("M-?" . lsp-find-references)
 	      ("C-c C-c l" . flycheck-list-errors)
 	      ("C-c C-c a" . lsp-execute-code-action)
-	      ("C-c C-c r" . lsp-reame)
+	      ("C-c C-c r" . lsp-rename)
 	      ("C-c C-c q" . lsp-workspace-restart)
 	      ("C-c C-c Q" . lsp-workspace-shutdown)
 	      ("C-c C-c s" . lsp-rust-analyzer-status)
