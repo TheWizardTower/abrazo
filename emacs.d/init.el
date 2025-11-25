c30eb9437b0d98581ae0053a16d9425cf73eaeba
Adam James McCullough <merlinfmct87@gmail.com>
2025-11-25 14:46:10 -0800

    Clean up the emacs init.el file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -12,6 +12,7 @@
 
 ;;; Contents:
 ;;;
+;;;  - Performance Tracking
 ;;;  - Basic settings
 ;;;  - Discovery aids
 ;;;  - Minibuffer/completion settings
@@ -23,30 +24,32 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
+;;;   Performance Tracking
+;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconst emacs-start-time (current-time))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
 ;;;   Basic settings
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Package initialization
-;;
-;; We'll stick to the built-in GNU and non-GNU ELPAs (Emacs Lisp Package
-;; Archive) for the base install, but there are some other ELPAs you could look
-;; at if you want more packages. MELPA in particular is very popular. See
-;; instructions at:
-;;
-;;    https://melpa.org/#/getting-started
-;;
-;; You can simply uncomment the following if you'd like to get started with
-;; MELPA packages quickly:
-;;
 (with-eval-after-load 'package
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
 
-;; If you want to turn off the welcome screen, uncomment this
-;;(setq inhibit-splash-screen t)
+;; Separate custom file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-(setq initial-major-mode 'fundamental-mode)  ; default mode for the *scratch* buffer
-(setq display-time-default-load-average nil) ; this information is useless for most
+;; Startup settings
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+(setq initial-major-mode 'fundamental-mode)
+(setq display-time-default-load-average nil)
 
 ;; Automatically reread from disk if the underlying file changes
 (setq auto-revert-interval 1)
@@ -55,6 +58,15 @@
 
 ;; Save history of minibuffer
 (savehist-mode)
+
+;; Recent files
+(use-package recentf
+  :demand t
+  :config
+  (recentf-mode 1)
+  (setq recentf-max-menu-items 25)
+  (setq recentf-max-saved-items 100)
+  :bind (("C-x C-r" . recentf-open-files)))
 
 ;; Move through windows with Ctrl-<arrow keys>
 (windmove-default-keybindings 'control) ; You can use other modifiers here
@@ -66,17 +78,22 @@
 (when (display-graphic-p)
   (context-menu-mode))
 
-;; Don't litter file system with *~ backup files; put them all inside
-;; ~/.emacs.d/backup or wherever
+;; Better backup configuration
 (defun bedrock--backup-file-name (fpath)
   "Return a new file path of a given file path.
 If the new path's directories does not exist, create them."
   (let* ((backupRootDir "~/.emacs.d/emacs-backup/")
-         (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path
+         (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath ))
          (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") )))
     (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
     backupFilePath))
+
 (setq make-backup-file-name-function 'bedrock--backup-file-name)
+(setq backup-by-copying t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -84,14 +101,10 @@ If the new path's directories does not exist, create them."
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Show the help buffer after startup
-;;(add-hook 'after-init-hook 'help-quick)
-
-;; which-key: shows a popup of available keybindings when typing a long key
-;; sequence (e.g. C-x ...)
 (use-package which-key
-  :ensure t
+  :demand t
   :config
+  (setq which-key-idle-delay 0.5)
   (which-key-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -100,28 +113,19 @@ If the new path's directories does not exist, create them."
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; For help, see: https://www.masteringemacs.org/article/understanding-minibuffer-completion
-
-(setq enable-recursive-minibuffers t)                ; Use the minibuffer whilst in the minibuffer
-(setq completion-cycle-threshold 1)                  ; TAB cycles candidates
-(setq completions-detailed t)                        ; Show annotations
-(setq tab-always-indent 'complete)                   ; When I hit TAB, try to complete, otherwise, indent
-(setq completion-styles '(basic initials substring)) ; Different styles to match input to candidates
-
-(setq completion-auto-help 'always)                  ; Open completion always; `lazy' another option
-(setq completions-max-height 20)                     ; This is arbitrary
+(setq enable-recursive-minibuffers t)
+(setq completion-cycle-threshold 1)
 (setq completions-detailed t)
+(setq tab-always-indent 'complete)
+(setq completion-styles '(basic initials substring))
+
+(setq completion-auto-help 'always)
+(setq completions-max-height 20)
 (setq completions-format 'one-column)
 (setq completions-group t)
-(setq completion-auto-select 'second-tab)            ; Much more eager
-;;(setq completion-auto-select t)                     ; See `C-h v completion-auto-select' for more possible values
+(setq completion-auto-select 'second-tab)
 
-(keymap-set minibuffer-mode-map "TAB" 'minibuffer-complete) ; TAB acts more like how it does in the shell
-
-;; For a fancier built-in completion option, try ido-mode or fido-mode. See also
-;; the file extras/base.el
-;;(fido-vertical-mode)
-;;(setq icomplete-delay-completions-threshold 4000)
+(keymap-set minibuffer-mode-map "TAB" 'minibuffer-complete)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
@@ -130,37 +134,32 @@ If the new path's directories does not exist, create them."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Mode line information
-(setq line-number-mode t)                        ; Show current line in modeline
-(setq column-number-mode t)                      ; Show column as well
+(setq line-number-mode t)
+(setq column-number-mode t)
 
-(setq x-underline-at-descent-line nil)           ; Prettier underlines
-(setq switch-to-buffer-obey-display-actions t)   ; Make switching buffers more consistent
+(setq x-underline-at-descent-line nil)
+(setq switch-to-buffer-obey-display-actions t)
 
-(setq-default show-trailing-whitespace nil)      ; By default, don't underline trailing spaces
-(setq-default indicate-buffer-boundaries 'left)  ; Show buffer top and bottom in the margin
+(setq-default show-trailing-whitespace nil)
+(setq-default indicate-buffer-boundaries 'left)
 
 ;; Enable horizontal scrolling
 (setq mouse-wheel-tilt-scroll t)
 (setq mouse-wheel-flip-direction t)
 
-;; We won't set these, but they're good to know about
-;;
-;; (setq-default indent-tabs-mode nil)
-;; (setq-default tab-width 4)
-
 ;; Misc. UI tweaks
-(blink-cursor-mode -1)                                ; Steady cursor
-(pixel-scroll-precision-mode)                         ; Smooth scrolling
+(blink-cursor-mode -1)
+(pixel-scroll-precision-mode)
 
 ;; Use common keystrokes by default
 (cua-mode)
 
 ;; Display line numbers in programming mode
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
-(setq-default display-line-numbers-width 3)           ; Set a minimum width
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(setq-default display-line-numbers-width 3)
 
 ;; Nice line wrapping when working with text
-(add-hook 'text-mode-hook 'visual-line-mode)
+(add-hook 'text-mode-hook #'visual-line-mode)
 
 ;; Modes to highlight the current line with
 (let ((hl-line-hooks '(text-mode-hook prog-mode-hook)))
@@ -189,6 +188,7 @@ If the new path's directories does not exist, create them."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package emacs
+  :demand t
   :config
   (load-theme 'modus-vivendi))          ; for light theme, use modus-operandi
 
@@ -198,11 +198,7 @@ If the new path's directories does not exist, create them."
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Uncomment the (load-file …) lines or copy code from the extras/ elisp files
-;; as desired
-
-;; UI/UX enhancements mostly focused on minibuffer and autocompletion interfaces
-;; These ones are *strongly* recommended!
+;; UI/UX enhancements - Vertico/Consult stack
 (load-file (expand-file-name "extras/base.el" user-emacs-directory))
 
 ;; Packages for software development
@@ -211,82 +207,36 @@ If the new path's directories does not exist, create them."
 ;; Vim-bindings in Emacs (evil-mode configuration)
 (load-file (expand-file-name "extras/vim-like.el" user-emacs-directory))
 
-;; Org-mode configuration
-;; WARNING: need to customize things inside the elisp file before use! See
-;; the file extras/org-intro.txt for help.
-;;(load-file (expand-file-name "extras/org.el" user-emacs-directory))
-
-;; Email configuration in Emacs
-;; WARNING: needs the `mu' program installed; see the elisp file for more
-;; details.
-;;(load-file (expand-file-name "extras/email.el" user-emacs-directory))
-
 ;; Tools for academic researchers
 (load-file (expand-file-name "extras/researcher.el" user-emacs-directory))
 
-;; inhibit startup message
-(setq inhibit-startup-message t)
+;; Org-mode configuration
+;; (load-file (expand-file-name "extras/org.el" user-emacs-directory))
 
+;; Email configuration
+;; (load-file (expand-file-name "extras/email.el" user-emacs-directory))
+
+;; Fortune cookie fun
 (use-package fortune-cookie
+  :demand t
   :config
   (setq fortune-cookie-cowsay-args  "-f tux -s")
   (fortune-cookie-mode))
 
-;; auto-format different source code files extremely intelligently
-;; https://github.com/radian-software/apheleia
+;; Auto-format different source code files
 (use-package apheleia
-  :ensure t
+  :demand t
   :config
   (apheleia-global-mode +1))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
-;;;   Built-in customization framework
+;;;   Startup time message
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(helm-minibuffer-history-key "M-p")
- '(package-selected-packages
-   '(apheleia bank-buddy beacon cape cargo cargo-mode cargo-transient
-	      citar-org-roam company-dict company-fuzzy company-irony
-	      company-irony-c-headers company-prescient
-	      company-quickhelp-terminal company-shell company-spell
-	      copilot copilot-chat corfu-terminal dashboard dirvish
-	      discover discover-my-major eat embark-consult evil-args
-	      evil-better-visual-line evil-collection evil-commentary
-	      evil-easymotion evil-escape evil-ex-shell-command
-	      evil-expat evil-extra-operator evil-fringe-mark
-	      evil-goggles evil-indent-plus evil-leader evil-lion
-	      evil-matchit evil-quickscope evil-smartparens evil-space
-	      evil-string-inflection evil-surround
-	      evil-terminal-cursor-changer evil-traces
-	      evil-vimish-fold evil-visual-mark-mode
-	      evil-visual-replace evil-visualstar flycheck-aspell
-	      flycheck-haskell flycheck-popup-tip flycheck-rust
-	      flycheck-status-emoji flycheck-yamllint flyspell-correct
-	      format-all fortune-cookie helm-dictionary
-	      helm-evil-markers highlight-indent-guides json-mode
-	      kind-icon line-reminder lsp-haskell lsp-metals lsp-ui
-	      magit-commit-mark magit-diff-flycheck magit-filenotify
-	      magit-find-file magit-todos marginalia mason nethack
-	      orderless projectile-codesearch projectile-ripgrep
-	      projectile-sift projectile-speedbar projectile-variable
-	      quelpa quotient rust-playground rustic sbt-mode shfmt
-	      sideline ssh-agency ssh-config-mode stripspace
-	      telephone-line termint terraform-mode time-zones
-	      toml-mode treesit-auto verse-mode vertico watch-buffer
-	      web-mode wgrep yaml-mode yasnippet))
- '(package-vc-selected-packages
-   '((copilot :url "https://github.com/copilot-emacs/copilot.el" :branch
-	      "main"))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs loaded in %.2fs with %d garbage collections."
+                     (float-time (time-subtract (current-time) emacs-start-time))
+                     gcs-done)))
