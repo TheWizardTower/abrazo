0ae96ca3721cf42f982f3827aa0be8d1d8ca7176
Adam James McCullough <merlinfmct87@gmail.com>
2025-12-11 19:02:06 -0800

    LSP, autocomplete, and lualine changes.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,33 +1,39 @@
 return {
-    -- nvim-cmp: Completion engine
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
+  -- nvim-cmp: Completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      -- Snippet engine
+      {
+        "L3MON4D3/LuaSnip",
+        build = "make install_jsregexp",
         dependencies = {
-            -- Snippet engine
-            {
-                "L3MON4D3/LuaSnip",
-                build = "make install_jsregexp",
-                dependencies = {
-                    -- Snippet collection
-                    "rafamadriz/friendly-snippets",
-                },
-                config = function()
-                    require("luasnip.loaders.from_vscode").lazy_load()
-                end,
-            },
-            -- Completion sources
-            "saadparwaiz1/cmp_luasnip",      -- luasnip source
-            "hrsh7th/cmp-nvim-lsp",          -- LSP source
-            "hrsh7th/cmp-buffer",            -- buffer words
-            "hrsh7th/cmp-path",              -- file paths
-            "hrsh7th/cmp-nvim-lua",          -- nvim lua API
-            "hrsh7th/cmp-cmdline",           -- cmdline completion
-            "hrsh7th/cmp-calc",              -- calculator
-            "f3fora/cmp-spell",              -- spell checking
-            -- Icons for completion menu
-            "onsails/lspkind.nvim",
+          -- Snippet collection
+          "rafamadriz/friendly-snippets",
         },
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+      },
+      -- Copilot integration with cmp
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+      -- Completion sources
+      "saadparwaiz1/cmp_luasnip",       -- luasnip source
+      "hrsh7th/cmp-nvim-lsp",           -- LSP source
+      "hrsh7th/cmp-buffer",             -- buffer words
+      "hrsh7th/cmp-path",               -- file paths
+      "hrsh7th/cmp-nvim-lua",           -- nvim lua API
+      "hrsh7th/cmp-cmdline",            -- cmdline completion
+      "hrsh7th/cmp-calc",               -- calculator
+      "f3fora/cmp-spell",               -- spell checking
+      -- Icons for completion menu
+      "onsails/lspkind.nvim",
     },
+  },
 }
-
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,60 +1,59 @@
 return {
-    {
-        'junnplus/lsp-setup.nvim',
-        dependencies = {
-            'neovim/nvim-lspconfig',
-            'williamboman/mason.nvim',                   -- optional
-            'WhoIsSethDaniel/mason-tool-installer.nvim', -- optional
-            'williamboman/mason-lspconfig.nvim',         -- optional
-        },
-        keys = {
-            { '<leader>f', ':lua vim.lsp.buf.format()' },
-        },
-    },
-
-    "neovim/nvim-lspconfig",
+  {
+    'junnplus/lsp-setup.nvim',
     dependencies = {
-        {
-            "SmiteshP/nvim-navbuddy",
-            dependencies = {
-                "SmiteshP/nvim-navic",
-                "nvimdev/lspsaga.nvim",
-                "MunifTanjim/nui.nvim"
-            },
-            opts = { lsp = { auto_attach = true } }
-        }
+      'neovim/nvim-lspconfig',
+      'williamboman/mason.nvim',                         -- optional
+      'WhoIsSethDaniel/mason-tool-installer.nvim',       -- optional
+      'williamboman/mason-lspconfig.nvim',               -- optional
     },
-    -- your lsp config or other stuff
+    keys = {
+      { '<leader>f', ':lua vim.lsp.buf.format()' },
+    },
+  },
 
-    "junegunn/fzf",
-    "junegunn/fzf.vim",
-    "gfanto/fzf-lsp.nvim",
-    "nvim-lua/plenary.nvim",
-    'RishabhRD/popfix',
-    'RishabhRD/nvim-lsputils',
-    'jubnzv/virtual-types.nvim',
+  "neovim/nvim-lspconfig",
+  dependencies = {
     {
-        "Dan7h3x/signup.nvim",
-        branch = "main",
-        opts = {
-            -- Your configuration options here
-        },
-        config = function(_, opts)
-            require("signup").setup(opts)
-        end
-    },
-    {
-        "williamboman/mason.nvim"
-    },
-    { "neoclide/coc.nvim" },
-    {
-        'nvimdev/lspsaga.nvim',
-        config = function()
-            require('lspsaga').setup({})
-        end,
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter', -- optional
-            'nvim-tree/nvim-web-devicons',     -- optional
-        },
+      "SmiteshP/nvim-navbuddy",
+      dependencies = {
+        "SmiteshP/nvim-navic",
+        "nvimdev/lspsaga.nvim",
+        "MunifTanjim/nui.nvim"
+      },
+      opts = { lsp = { auto_attach = true } }
     }
+  },
+  -- your lsp config or other stuff
+
+  "junegunn/fzf",
+  "junegunn/fzf.vim",
+  "gfanto/fzf-lsp.nvim",
+  "nvim-lua/plenary.nvim",
+  'RishabhRD/popfix',
+  'RishabhRD/nvim-lsputils',
+  'jubnzv/virtual-types.nvim',
+  {
+    "Dan7h3x/signup.nvim",
+    branch = "main",
+    opts = {
+      -- Your configuration options here
+    },
+    config = function(_, opts)
+      require("signup").setup(opts)
+    end
+  },
+  {
+    "williamboman/mason.nvim"
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+      require('lspsaga').setup({})
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',       -- optional
+      'nvim-tree/nvim-web-devicons',           -- optional
+    },
+  }
 }
