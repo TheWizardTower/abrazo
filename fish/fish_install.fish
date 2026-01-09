ec1e789c45f2905ccf5221428734d3caa537365d
Adam James McCullough <merlinfmct87@gmail.com>
2026-01-09 14:51:50 -0800

    Format pass.

--- fish/fish_install.fish
+++ fish/fish_install.fish
@@ -1,27 +1,27 @@
 #!/usr/bin/env fish
 
 set -l conf_files alias budspencer direnv ls-color ssh-agent zz_fortune
-for file in $conf_files
-	echo $file
-	if test ! -e ~/.config/fish/conf.d/$file.fish
-		ln -s ~/abrazo/fish/conf.d/$file.fish ~/.config/fish/conf.d/$file.fish
-        end
+for path_entry in $conf_files
+    echo $path_entry
+    if test ! -e ~/.config/fish/conf.d/"$path_entry".fish
+        ln -s ~/abrazo/fish/conf.d/"$path_entry".fish ~/.config/fish/conf.d/"$path_entry".fish
+    end
 end
 
 for path_entry in (ls -1 paths.d)
-	echo $path_entry
-	if test ! -e ~/.config/fish/paths.d/$file
-		ln -s ~/abrazo/fish/paths.d/$file ~/.config/fish/conf.d/$file
-        end
+    echo $path_entry
+    if test ! -e ~/.config/fish/paths.d/$file
+        ln -s ~/abrazo/fish/paths.d/$file ~/.config/fish/conf.d/$file
+    end
 end
 
-if ! fish -c "fisher --help" > /dev/null
-	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish \
-		| source \
-		&& fisher install jorgebucaran/fisher
+if ! fish -c "fisher --help" >/dev/null
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish \
+        | source \
+        && fisher install jorgebucaran/fisher
 end
 
 for package in (cat fishfile)
-	echo $package
-	fisher install $package
+    echo $package
+    fisher install $package
 end
