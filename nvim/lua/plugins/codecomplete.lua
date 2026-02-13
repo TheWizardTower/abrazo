18121ae5548afd8d80fe6c910cf560722eda9489
Adam James McCullough <merlinfmct87@gmail.com>
2026-02-12 18:01:34 -0800

    Add flash, precognition, code-complete, and neogit plugins

---
+++ nvim/lua/plugins/codecomplete.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require('config.codecomplete')
+    end,
+  },
+}
---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    config = function()
+      require('config.flash')
+    end,
+  },
+}
