77671478445297fdb1833640e464d412ebd6518f
adam@cravin.com <adam@cravin.com>
2026-03-04 21:28:52 -0800

    feat(cargo_packages) add packages to cargo setup script

--- cargo_packages.sh
+++ cargo_packages.sh
@@ -10,24 +10,46 @@ fi
 
 export PACKAGE_LIST="
   aichat \
+  argc \
   bat \
+  caargo-outdated \
+  cargo-llvm-cov \
   code2prompt \
   erdtree \
   exa \
   fd-find \
+  flamegraph \
+  git-delta \
   leadr \
+  mergiraf \
   nu \
+  oha \
+  pier \
   procs \
   ripgrep \
   sd \
   tealdeer \
   tenere \
+  tokio-console
   tre \
+  varisat-cli \
   xh \
   yj \
   zoxide \
   "
 
+export LOCKED_PACKAGE_LIST="
+  bacon-ls \
+  difftastic \
+  jj \
+  jj-cli \
+  zellij \
+"
+
 for package in $PACKAGE_LIST; do
   cargo install $package
+done
+
+for package in $LOCKED_PACKAGE_LIST; do
+  cargo install --locked $package
 done
