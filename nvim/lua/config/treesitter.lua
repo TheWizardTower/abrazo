ccaf482a722171a094055b83ec10e3f12d5980d3
Adam James McCullough <merlinfmct87@gmail.com>
2026-03-09 19:18:50 -0700

    refactor(nvim/treesitter): unify highlight disable into one function

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -47,11 +47,14 @@ require 'nvim-treesitter.configs'.setup {
         -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
         -- the name of the parser)
-        -- list of language that will be disabled
-        disable = { "c" },
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        -- Disable treesitter for specific languages and large files
         disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
+            -- Disable for C
+            if lang == "c" then
+                return true
+            end
+            -- Disable for large files (>100 KB)
+            local max_filesize = 100 * 1024
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
                 return true
@@ -62,6 +65,6 @@ require 'nvim-treesitter.configs'.setup {
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = true,
+        additional_vim_regex_highlighting = false,
     },
 }
