4d48ea45d36aa6ab89fd1692169791b53fca221a
Adam James McCullough <merlinfmct87@gmail.com>
2026-03-09 19:18:50 -0700

    refactor(nvim): consolidate which-key into a single plugin spec

--- nvim/lua/plugins/code-feedback.lua
+++ nvim/lua/plugins/code-feedback.lua
@@ -1,33 +1,7 @@
 return {
   -- Preview nvim registers.
   { 'gennaro-tedesco/nvim-peekup' },
-  -- Show possible keybindings in popup.
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-    },
-  },
 
   { 'nvim-treesitter/highlight.lua' },
   { 'nvim-treesitter/nvim-treesitter' },
-
-  {
-    'hrsh7th/cmp-nvim-lsp',
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    event = { "InsertEnter", "CmdlineEnter" }
-  },
-  { 'onsails/lspkind.nvim' },
 }
