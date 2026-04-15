ba57b2006c5c14c440a1d622aa951b96e99f7366
Adam James McCullough <merlinfmct87@gmail.com>
2026-04-15 11:25:31 -0700

    fix(ci) emacs in GHA doesn't understand crazy GUI moon language

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -34,7 +34,7 @@
 
 ;; Default frame configuration: full screen, good-looking title bar on macOS
 (setq frame-resize-pixelwise t)
-(tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))  ; All these tools are in the menu-bar anyway
 (setq default-frame-alist '((fullscreen . maximized)
                             ;; You can turn off scroll bars by uncommenting these lines:
                             ;; (vertical-scroll-bars . nil)
