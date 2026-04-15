6085f5139df387fe0fff2a4d3c7d6cf7bb86d44d
Adam James McCullough <merlinfmct87@gmail.com>
2026-04-15 09:20:42 -0700

    feat(nvim): extend telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,12 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-live-grep-args.nvim',
+    },
     config = function()
         require('config.telescope')
+        require('telescope').load_extension('live_grep_args')
     end,
 }
