03692ab142d9c999f019d5cbfcec44ce554be340
Adam James McCullough <merlinfmct87@gmail.com>
2026-04-15 09:20:42 -0700

    feat(nvim) surround

---
+++ nvim/lua/plugins/surround.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+    },
+}
