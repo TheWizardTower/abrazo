1f02aae9726c2c4f60c15c3d2957124a475e4038
Adam McCullough <merlinfmct87@gmail.com>
2026-07-30 21:20:09 -0700

    feat(nvim): telescope setup, hotkeys

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,9 +4,18 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-telescope/telescope-live-grep-args.nvim',
+        -- Additional useful extensions
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },  -- faster fuzzy finding
     },
     config = function()
         require('config.telescope')
+        
+        -- Load extensions
         require('telescope').load_extension('live_grep_args')
+        
+        -- Try to load fzf-native if available (for faster sorting)
+        pcall(function()
+            require('telescope').load_extension('fzf')
+        end)
     end,
 }
